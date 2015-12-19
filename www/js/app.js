64cba29786dd3b05161c6c9c2168548d6e7340f0
alsbtain <info@alsbtain.net>
2015-12-19 12:26:11 -0800

    debugdebug

--- www/js/app.js
+++ www/js/app.js
@@ -49,20 +49,10 @@ $.usersession = {
                     console.log(idx);
 					
 				pushID = idx;
-				alert(pushID);
 				
-				try 
-				{ 
 					pushNotification = window.plugins.pushNotification;
-					if (device.platform == 'android' || device.platform == 'Android') {
-						//$("#app-status-ul").append('<li>registering android</li>');
-						pushNotification.register(successHandler, errorHandler, {"senderID":"31755832513","ecb":"onNotificationGCM"});		// required!
-					} else {
-						//$("#app-status-ul").append('<li>registering iOS</li>');
-						pushNotification.register(tokenHandler, errorHandler, {"badge":"true","sound":"true","alert":"true","ecb":"onNotificationAPN"});	// required!
-					}
-				}
-
+					pushNotification.register(successHandler, errorHandler, {"senderID":"31755832513","ecb":"onNotificationGCM"});
+				
 				}
 				
 	            
